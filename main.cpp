109165b5f3aa09eff26883ce23eae1285dc898a7
darcs <vcs@elidupree.com>
2011-12-23 00:48:50 +0000

    first record, we have a shitty pressure-only granular thing

---
+++ main.cpp
@@ -0,0 +1,279 @@
+
+/* Simple program:  Create a blank window, wait for keypress, quit.
+
+   Please see the SDL documentation for details on using the SDL API:
+   /Developer/Documentation/SDL/docs.html
+*/
+   
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#include "SDL.h"
+#include "GL/gl.h"
+#include "GL/glu.h"
+
+#include "water.cpp"
+#include <iostream>
+
+static SDL_Surface *gScreen;
+
+static void initAttributes ()
+{
+    // Setup attributes we want for the OpenGL context
+    
+    int value;
+    
+    // Don't set color bit sizes (SDL_GL_RED_SIZE, etc)
+    //    Mac OS X will always use 8-8-8-8 ARGB for 32-bit screens and
+    //    5-5-5 RGB for 16-bit screens
+    
+    // Request a 16-bit depth buffer (without this, there is no depth buffer)
+    value = 16;
+    SDL_GL_SetAttribute (SDL_GL_DEPTH_SIZE, value);
+    
+    
+    // Request double-buffered OpenGL
+    //     The fact that windows are double-buffered on Mac OS X has no effect
+    //     on OpenGL double buffering.
+    value = 1;
+    SDL_GL_SetAttribute (SDL_GL_DOUBLEBUFFER, value);
+}
+
+static void printAttributes ()
+{
+    // Print out attributes of the context we created
+    int nAttr;
+    int i;
+    
+    int  attr[] = { SDL_GL_RED_SIZE, SDL_GL_BLUE_SIZE, SDL_GL_GREEN_SIZE,
+                    SDL_GL_ALPHA_SIZE, SDL_GL_BUFFER_SIZE, SDL_GL_DEPTH_SIZE };
+                    
+    char *desc[] = { "Red size: %d bits\n", "Blue size: %d bits\n", "Green size: %d bits\n",
+                     "Alpha size: %d bits\n", "Color buffer size: %d bits\n", 
+                     "Depth bufer size: %d bits\n" };
+
+    nAttr = sizeof(attr) / sizeof(int);
+    
+    for (i = 0; i < nAttr; i++) {
+    
+        int value;
+        SDL_GL_GetAttribute ((SDL_GLattr)attr[i], &value);
+        printf (desc[i], value);
+    } 
+}
+
+static void createSurface (int fullscreen)
+{
+    Uint32 flags = 0;
+    
+    flags = SDL_OPENGL;
+    if (fullscreen)
+        flags |= SDL_FULLSCREEN;
+    
+    // Create window
+    gScreen = SDL_SetVideoMode (640, 640, 0, flags);
+    if (gScreen == NULL) {
+		
+        fprintf (stderr, "Couldn't set 640x640 OpenGL video mode: %s\n",
+                 SDL_GetError());
+		SDL_Quit();
+		exit(2);
+	}
+}
+
+static void mainLoop ()
+{
+    SDL_Event event;
+    int done = 0;
+    int frame = 0;
+    int p_mode = 0;
+
+	for (EACH_LOCATION(loc))
+	{
+	tiles[loc].contents = AIR;
+	}
+	for (EACH_LOCATION(loc))
+	{
+		if (loc.x >= 4 && loc.x <= 6 && loc.y >= 4 && loc.y <= 6 && loc.z >= 1/* && loc.z <= 6*/ )tiles[loc].contents = WATER; //* 4 / 5;
+		//else if (loc.x == 5 && loc.y >= 5 && loc.z == 0) ;
+		else if (loc.x >= 3 && loc.x <= 7 && loc.y >= 3 && loc.y <= 7 && loc.z >= 1)tiles[loc].contents = ROCK;
+		else if ((loc.x == 1 || loc.x == 9 || loc.y == 1 || loc.y == 9) && loc.z < 3)tiles[loc].contents = ROCK;
+		//if (loc.x >= 5 && loc.x <= 11 && loc.y >= 5 && loc.y <= 11 && loc.z < 1)tiles[loc].water.amount = precision_scale * 4 / 5;
+		/*if (loc.z == 0 && loc.x >= 5) tiles[loc].is_solid_rock =true;
+		else if (loc.z == 1 && loc.x >= 10) tiles[loc].is_solid_rock =true;
+		else if (loc.z == 2 && loc.x >= 15) tiles[loc].is_solid_rock =true;
+		else if (loc.x == 19)tiles[loc].water.amount = precision_scale;*/
+		/*if (loc.z < 8 && loc.x > 10) tiles[loc].is_solid_rock =true;
+		else if (loc.x >= 12 && loc.y <= 13 && loc.y >= 7) { tiles[loc].water.amount = precision_scale; }
+		else if (loc.y != 10 && loc.x > 10) tiles[loc].is_solid_rock =true;
+		else if (loc.z > 8 && loc.x > 10 && loc.x < 18) tiles[loc].is_solid_rock =true;
+		else if (loc.x > 10) { tiles[loc].water.amount = precision_scale; }*/
+		/*if (loc.z < 20 - loc.x) tiles[loc].is_solid_rock = true;
+		else if (loc.z >= 15 && (20 - loc.x) >= 15) tiles[loc].water.amount = precision_scale;*/
+		/*if (loc.x == 0)tiles[loc].water.amount = precision_scale;
+		else if (loc.x == 1 && loc.z > 0) tiles[loc].is_solid_rock = true;
+		else if (loc.x == 5) tiles[loc].is_solid_rock = true;
+		else if (loc.x == 2 && (loc.z % 4) == 1) tiles[loc].is_solid_rock = true;
+		else if (loc.x == 3 && (loc.z % 2) == 1) tiles[loc].is_solid_rock = true;
+		else if (loc.x == 4 && (loc.z % 4) == 3) tiles[loc].is_solid_rock = true;*/
+	}
+    
+    while ( !done ) {
+
+		/* Check for events */
+		while ( SDL_PollEvent (&event) ) {
+			switch (event.type) {
+
+				case SDL_MOUSEMOTION:
+					break;
+				case SDL_MOUSEBUTTONDOWN:
+					break;
+				case SDL_KEYDOWN:
+					if(event.key.keysym.sym == SDLK_p) ++p_mode;
+					if(event.key.keysym.sym != SDLK_ESCAPE)break;
+				case SDL_QUIT:
+					done = 1;
+					break;
+				default:
+					break;
+			}
+		}
+		if(p_mode == 1)continue;
+		if(p_mode > 1)--p_mode;
+		int before_drawing = SDL_GetTicks();
+		
+		glClear(GL_COLOR_BUFFER_BIT);
+		
+		//drawing code here
+		frame += 1;
+	glLoadIdentity();
+	gluPerspective(80, 1, 1, 100);
+	gluLookAt(5.0 + 20.0 * std::cos((double)frame / 40.0),5.0 + 20.0 * std::sin((double)frame / 40.0),15.0 + 5.0 * std::sin((double)frame / 60.0),5,5,5,0,0,1);
+		int total_amount = 0;
+		int max_pressure = 0;
+		int total_force = 0;
+		int max_velmgsq = 0;
+	for (EACH_LOCATION(loc))
+	{
+		if(loc.z == 0){
+		glColor4f(0.2,0.4,0.0,1.0);
+
+		glBegin(GL_POLYGON);
+			glVertex3f(loc.x, loc.y, loc.z);
+			glVertex3f(loc.x + 1, loc.y, loc.z);
+			glVertex3f(loc.x + 1, loc.y +1, loc.z);
+			glVertex3f(loc.x, loc.y+1, loc.z);
+		glEnd();
+		}
+	}
+	for (EACH_LOCATION(loc))
+	{
+		if (tiles[loc].contents != AIR)
+		{
+		  if (tiles[loc].contents == ROCK) {
+		glColor4f(0.5,0.0,0.0,0.5);
+		}
+		else {
+		  glColor4f(0.0, 0.0, 1.0, 0.5);
+		}
+		glBegin(GL_POLYGON);
+			glVertex3f(loc.x, loc.y, (double)loc.z + 0.5);
+			glVertex3f(loc.x + 1, loc.y, (double)loc.z + 0.5);
+			glVertex3f(loc.x + 1, loc.y +1, (double)loc.z + 0.5);
+			glVertex3f(loc.x, loc.y+1, (double)loc.z + 0.5);
+		glEnd();
+		}
+		#if 0
+		if (local_amount < precision_scale / 100) continue;
+//std::cerr << local_amount << "\n";
+
+//std::cerr << tiles[loc].water.velocity_delay.x << " " << tiles[loc].water.velocity_delay.y << " " << tiles[loc].water.velocity_delay.z << " " << "\n";
+		glColor4f(0.0,0.0,1.0,(double)local_amount / (double)(precision_scale*2));
+		glBegin(GL_POLYGON);
+			glVertex3f(loc.x, loc.y, (double)loc.z + 0.5);
+			glVertex3f(loc.x + 1, loc.y, (double)loc.z + 0.5);
+			glVertex3f(loc.x + 1, loc.y +1, (double)loc.z + 0.5);
+			glVertex3f(loc.x, loc.y+1, (double)loc.z + 0.5);
+		glEnd();
+		glColor4f(0.0,0.0,1.0,((double)local_amount + (double)precision_scale) / (double)(precision_scale*2));
+		glBegin(GL_LINES);
+			glVertex3f((double)loc.x+0.5, (double)loc.y+0.5, (double)loc.z + 0.5);
+			glVertex3f((double)loc.x+0.5+((double)tiles[loc].water.velocity_delay.x/(double)precision_scale), (double)loc.y+0.5+((double)tiles[loc].water.velocity_delay.y/(double)precision_scale), (double)loc.z + 0.5+((double)tiles[loc].water.velocity_delay.z/(double)precision_scale));
+		glEnd();
+		glColor4f(0.0,1.0,0.0,((double)local_amount + (double)precision_scale) / (double)(precision_scale*2));
+		glBegin(GL_LINES);
+			glVertex3f((double)loc.x+0.5, (double)loc.y+0.5, (double)loc.z + 0.5);
+			glVertex3f((double)loc.x+0.5+((double)tiles[loc].water.velocity.x/(double)precision_scale), (double)loc.y+0.5+((double)tiles[loc].water.velocity.y/(double)precision_scale), (double)loc.z + 0.5+((double)tiles[loc].water.velocity.z/(double)precision_scale));
+		glEnd();
+		glBegin(GL_LINES);
+		glColor4f(0.8,0.8,0.0,((double)local_amount + (double)precision_scale) / (double)(precision_scale*2));
+			for(EACH_DIRECTION(d))
+			{
+				const scalar_type force_we = (outpushing_velocity[loc.x][loc.y][loc.z][1+d.x][1+d.y][1+d.z]);
+				//std::cerr<<force_we;
+				glVertex3f((double)loc.x+0.5, (double)loc.y+0.5, (double)loc.z + 0.5);
+				glVertex3f((double)loc.x+0.5+((double)force_we*d.x/(double)precision_scale), (double)loc.y+0.5+((double)force_we*d.y/(double)precision_scale), (double)loc.z + 0.5+((double)force_we*d.z/(double)precision_scale));
+			}
+		glEnd();
+		glColor4f(1.0,0.0,0.0,((double)local_amount + (double)precision_scale) / (double)(precision_scale*2));
+		glBegin(GL_LINES);
+			glVertex3f((double)loc.x+0.5, (double)loc.y+0.5, (double)loc.z + 0.5);
+			glVertex3f((double)loc.x+0.5+((double)tiles[loc].water.pressure/(double)precision_scale), (double)loc.y+0.5, (double)loc.z + 0.5+((double)tiles[loc].water.pressure/(double)precision_scale));
+		glEnd();
+		max_pressure = std::max(max_pressure, (int)tiles[loc].water.pressure);
+		max_velmgsq = std::max(max_velmgsq, (int)(tiles[loc].water.velocity.dot(tiles[loc].water.velocity)));
+		total_force += tiles[loc].water.total_force;
+
+	#endif
+//std::cerr << total_amount << "---"<<max_pressure << "---"<<total_force << "\n";
+	}
+		glFinish();	
+        SDL_GL_SwapBuffers();
+		int before_processing = SDL_GetTicks();
+			std::cerr << time << "\n";
+		
+		//doing stuff code here
+		update_water();
+		int after = SDL_GetTicks();
+std::cerr << (after - before_processing) << ", " << (before_processing - before_drawing) << "\n";
+
+//	SDL_Delay(50);
+		
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	// Init SDL video subsystem
+	if ( SDL_Init (SDL_INIT_VIDEO) < 0 ) {
+		
+        fprintf(stderr, "Couldn't initialize SDL: %s\n",
+			SDL_GetError());
+		exit(1);
+	}
+
+    // Set GL context attributes
+    initAttributes ();
+    
+    // Create GL context
+    createSurface (0);
+    
+    // Get GL context attributes
+    printAttributes ();
+    
+    // Init GL state
+	gluPerspective(90, 1, 1, 100);
+	gluLookAt(20,20,20,0,0,0,0,0,1);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA);
+    
+    // Draw, get events...
+    mainLoop ();
+    
+    // Cleanup
+	SDL_Quit();
+	
+    return 0;
+}
---
+++ water.cpp
@@ -0,0 +1,293 @@
+
+#include <vector>
+#include <cstdlib>
+#include <cmath>
+#include <cassert>
+#include <map>
+#include <set>
+
+#include <iostream>
+#include <inttypes.h>
+
+class bounds_checked_int {
+public:
+	bounds_checked_int():value(0){}
+	bounds_checked_int(int value):value(value){}
+	bounds_checked_int &operator=(int other) { value = other; return *this; }
+	bounds_checked_int operator+(int other)const {
+		assert((int64_t)value + (int64_t)other < (1LL << 31));
+		assert((int64_t)value + (int64_t)other > -(1LL << 31));
+		return bounds_checked_int(value + other);
+	}
+	void operator+=(int other) {
+		assert((int64_t)value + (int64_t)other < (1LL << 31));
+		assert((int64_t)value + (int64_t)other > -(1LL << 31));
+		value += other;
+	}
+	bounds_checked_int& operator++() {
+		*this += 1; return *this;
+	}
+	bounds_checked_int operator++(int) {
+		bounds_checked_int result(value);
+		*this += 1;
+		return result;
+	}
+	bounds_checked_int& operator--() {
+		*this -= 1; return *this;
+	}
+	bounds_checked_int operator--(int) {
+		bounds_checked_int result(value);
+		*this -= 1;
+		return result;
+	}
+	bounds_checked_int operator-(int other)const {
+		assert((int64_t)value - (int64_t)other < (1LL << 31));
+		assert((int64_t)value - (int64_t)other > -(1LL << 31));
+		return bounds_checked_int(value - other);
+	}
+	void operator-=(int other) {
+		assert((int64_t)value - (int64_t)other < (1LL << 31));
+		assert((int64_t)value - (int64_t)other > -(1LL << 31));
+		value -= other;
+	}
+	bounds_checked_int operator*(int other)const {
+		assert((int64_t)value * (int64_t)other < (1LL << 31));
+		assert((int64_t)value * (int64_t)other > -(1LL << 31));
+		return bounds_checked_int(value * other);
+	}
+	void operator*=(int other) {
+		assert((int64_t)value * (int64_t)other < (1LL << 31));
+		assert((int64_t)value * (int64_t)other > -(1LL << 31));
+		value *= other;
+	}
+	bounds_checked_int operator/(int other)const {
+		return bounds_checked_int(value / other);
+	}
+	void operator/=(int other) {
+		value /= other;
+	}
+	operator int()const{ return value; }
+private:
+	int value;
+};
+
+//typedef bounds_checked_int scalar_type;
+//typedef int64_t scalar_type;
+typedef int scalar_type;
+
+int divide_rounding_towards_zero(int dividend, int divisor)
+{
+	assert(divisor != 0);
+	int abs_result = std::abs(dividend) / std::abs(divisor);
+	if ((dividend > 0 && divisor > 0) || (dividend < 0 && divisor < 0)) return abs_result;
+	else return -abs_result;
+}
+
+int sign(int number)
+{
+	if (number > 0) return 1;
+	if (number < 0) return -1;
+	return 0;
+}
+
+
+class vector3 {
+public:
+	scalar_type x, y, z;
+	vector3():x(0),y(0),z(0){}
+	vector3(int x, int y, int z):x(x),y(y),z(z){}
+	vector3 operator+(vector3 const& other)const {
+		return vector3(x + other.x, y + other.y, z + other.z);
+	}
+	void operator+=(vector3 const& other) {
+		x += other.x; y += other.y; z += other.z;
+	}
+	vector3 operator-(vector3 const& other)const {
+		return vector3(x - other.x, y - other.y, z - other.z);
+	}
+	void operator-=(vector3 const& other) {
+		x -= other.x; y -= other.y; z -= other.z;
+	}
+	vector3 operator*(int other)const {
+		return vector3(x * other, y * other, z * other);
+	}
+	void operator*=(int other) {
+		x *= other; y *= other; z *= other;
+	}
+	vector3 operator/(int other)const {
+		return vector3(divide_rounding_towards_zero(x, other), divide_rounding_towards_zero(y, other), divide_rounding_towards_zero(z, other));
+	}
+	void operator/=(int other) {
+		x = divide_rounding_towards_zero(x, other); y = divide_rounding_towards_zero(y, other); z = divide_rounding_towards_zero(z, other);
+	}
+	vector3 operator-()const { // unary minus
+		return vector3(-x, -y, -z);
+	}
+	bool operator==(vector3 const& other)const {return x == other.x && y == other.y && z == other.z; }
+	bool operator!=(vector3 const& other)const {return x != other.x || y != other.y || z != other.z; }
+	scalar_type dot(vector3 const& other)const {
+		return x * other.x + y * other.y + z * other.z;
+	}
+	scalar_type magnitude()const { return std::sqrt((int)dot(*this));}
+	bool operator<(vector3 const& other)const { return (x < other.x) || ((x == other.x) && ((y < other.y) || ((y == other.y) && (z < other.z)))); }
+};
+
+typedef vector3 one_tile_direction_vector;
+typedef vector3 location;
+
+#define MAX_X 20
+#define MAX_Y 20
+#define MAX_Z 20
+
+void incr_location(location &foo){
+	++foo.x;
+	if (foo.x >= MAX_X) { foo.x = 0; ++foo.y; }
+	if (foo.y >= MAX_Y) { foo.y = 0; ++foo.z; }
+}
+void decr_location(location &foo){
+	--foo.x;
+	if (foo.x < 0) { foo.x = MAX_X - 1; --foo.y; }
+	if (foo.y < 0) { foo.y = MAX_Y - 1; --foo.z; }
+}
+void incr_direction(one_tile_direction_vector &foo){
+	++foo.x;
+	if (foo.x == 0 && foo.y == 0 && foo.z == 0) ++foo.x;
+	if (foo.x >= 2) { foo.x = -1; ++foo.y; }
+	if (foo.y >= 2) { foo.y = -1; ++foo.z; }
+}
+void incr_cardinal_direction(one_tile_direction_vector &foo){
+	     if (foo.x == -1) foo = vector3( 0,-1, 0);
+	else if (foo.y == -1) foo = vector3( 0, 0,-1);
+	else if (foo.z == -1) foo = vector3( 1, 0, 0);
+	else if (foo.x == 1) foo = vector3( 0, 1, 0);
+	else if (foo.y == 1) foo = vector3( 0, 0, 1);
+	else if (foo.z == 1) foo = vector3( -2, 0, 0); // hack - "invalid value"
+}
+
+#define EACH_LOCATION(varname) location varname(0,0,0); varname.z < MAX_Z; incr_location(varname)
+#define EACH_LOCATION_REVERSE(varname) location varname(MAX_X - 1,MAX_Y - 1,MAX_Z - 1); varname.z >= 0; decr_location(varname)
+#define EACH_DIRECTION(varname) one_tile_direction_vector varname(-1,-1,-1); varname.z < 2; incr_direction(varname)
+#define EACH_CARDINAL_DIRECTION(varname) one_tile_direction_vector varname(-1,0,0); varname.x != -2; incr_cardinal_direction(varname)
+
+//const scalar_type precision_scale = 1000;
+// This constant makes an object be travelling at one tile per frame when it hits the ground after a 100 tile fall.
+//vector3 default_gravity_in_subdivisions_per_frame_squared(0, 0, -(precision_scale / 200));
+//#define NUM_WATER_UNITS
+
+#if 0
+scalar_type stupid_distance(one_tile_direction_vector d)
+{
+	int grid_dist = std::abs(d.x) + std::abs(d.y) + std::abs(d.z);
+	     if (grid_dist == 0)  return 0;
+	else if (grid_dist == 1)  return precision_scale;
+	else if (grid_dist == 2)  return precision_scale *  92682 / (1 << 16);
+	else /*(grid_dist == 3)*/ return precision_scale * 113512 / (1 << 16);
+}
+#endif
+
+
+/*struct water_tile
+{
+	vector3 velocity[NUM_WATER_UNITS]; // in tiles per frame, scaled up by precision_scale
+	vector3 progress[NUM_WATER_UNITS]; // From -precision_scale/2 (bordering the negative-direction tile) to precision_scale/2 (bordering the positive-direction tile).
+	scalar_type pressure;
+	scalar_type amount;
+	water_tile():velocity(0,0,0),total_force(0),amount(0){}
+};*/
+
+enum tile_contents {
+  ROCK,
+  WATER,
+  AIR
+};
+
+struct tile
+{
+  tile_contents contents;
+  int water_group_number; // a temporary variable during the computation
+};
+
+bool out_of_bounds(location loc){return loc.x < 0 || loc.y < 0 || loc.z < 0 || loc.x >= MAX_X || loc.y >= MAX_Y || loc.z >= MAX_Z; }
+
+struct tiles_type {
+	tile tiles[MAX_X][MAX_Y][MAX_Z];
+	tile& operator[](location loc){ assert(!out_of_bounds(loc)); return tiles[loc.x][loc.y][loc.z]; }
+	tile const& operator[](location loc)const { assert(!out_of_bounds(loc)); return tiles[loc.x][loc.y][loc.z]; }
+};
+tiles_type tiles;
+
+// 'velocity' in a diagonal direction is scaled up (i.e. velocity going to (1,1,1) is sqrt(3) times as gret as the same amount going in a cardinal direction)
+scalar_type outpushing_velocity[MAX_X][MAX_Y][MAX_Z][3][3][3];
+scalar_type outgoing_forces_from_pressure[MAX_X][MAX_Y][MAX_Z][3][3][3];
+scalar_type outgoing_water[MAX_X][MAX_Y][MAX_Z][3][3][3];
+
+
+
+void mark_water_group_that_includes(location loc, int group_number, std::map<int, std::set<location> >& edge_tiles_by_group) {
+  std::set<location> frontier;
+  frontier.insert(loc);
+  edge_tiles_by_group.insert(std::make_pair(group_number, std::set<location>()));
+  while(!frontier.empty())
+  {
+    const location next_loc = *(frontier.begin());
+    frontier.erase(frontier.begin());
+    tiles[next_loc].water_group_number = group_number;
+    bool is_edge_tile = false;
+    for (EACH_CARDINAL_DIRECTION(dir)) {
+      const location adj_loc = next_loc + dir;
+      if (!out_of_bounds(adj_loc)) {
+        tile &adj_tile = tiles[adj_loc];
+        if (adj_tile.contents == WATER && adj_tile.water_group_number == 0) {
+          frontier.insert(adj_loc);
+        }
+        if (adj_tile.contents == AIR) {
+         is_edge_tile = true;
+        }
+      }
+    }
+    if (is_edge_tile) edge_tiles_by_group[group_number].insert(next_loc);
+  }
+}
+
+void update_water() {
+  for (EACH_LOCATION(loc)) {
+    tiles[loc].water_group_number = 0;
+  }
+  int next_group_number = 1;
+  std::map<int, std::set<location> > edge_tiles_by_group;
+  for (EACH_LOCATION(loc)) {
+    if(tiles[loc].contents == WATER && tiles[loc].water_group_number == 0) {
+      mark_water_group_that_includes(loc, ++next_group_number, edge_tiles_by_group);
+    }
+  }
+  for (std::map<int, std::set<location> >::const_iterator i = edge_tiles_by_group.begin(); i != edge_tiles_by_group.end(); ++i) {
+    const int group_number = i->first;
+    const std::set<location> edge_tiles = i->second;
+    int max_z = 0;
+    std::vector<location> top_tiles;
+    for (std::set<location>::const_iterator j = edge_tiles.begin(); j != edge_tiles.end(); ++j) {
+      if (j->z > max_z) max_z = j->z;
+    }
+    std::set<std::pair<location, one_tile_direction_vector> > non_top_surfaces;
+    for (std::set<location>::const_iterator j = edge_tiles.begin(); j != edge_tiles.end(); ++j) {
+      if (j->z == max_z) top_tiles.push_back(*j);
+      for (EACH_CARDINAL_DIRECTION(dir)) {
+        if (!(j->z == max_z && dir.z == 1)) {
+          const location adj_loc = *j + dir;
+          if (!out_of_bounds(adj_loc)) {
+            if (tiles[adj_loc].contents == AIR) non_top_surfaces.insert(std::make_pair(*j, dir));
+          }
+        }
+      }
+    }
+    
+    for(std::set<std::pair<location, one_tile_direction_vector> >::const_iterator surface = non_top_surfaces.begin(); surface != non_top_surfaces.end(); ++surface) {
+      if (rand()%10000 < 10 * std::sqrt(max_z + 0.5 - ((double)surface->first.z + 0.5*surface->second.z))) {
+        tiles[top_tiles[rand()%top_tiles.size()]].contents = AIR;
+        tiles[surface->first + surface->second].contents = WATER;
+        break;
+      }
+    }
+  }
+}
+
