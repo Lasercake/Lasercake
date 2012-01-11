dba0fdbf6154268bdf740885a99401cc8591d32c
Isaac Dupree <antispam@idupree.com>
2012-01-11 01:29:18 -0500

    Draw laserbeams in more the right order compared to everything else.

--- main.cpp
+++ main.cpp
@@ -361,28 +361,37 @@ srand(time(NULL));
     }
     
     for (auto p : w.laser_sfxes) {
-      vertices_t& vertices = verticeses[0]; //TODO choose better
       vector3<GLfloat> locv = convert_coordinates_to_GL(view_loc, p.first);
       vector3<GLfloat> locv2 = convert_coordinates_to_GL(view_loc, p.first + p.second);
       //std::cerr << locv.x << " !l " << locv.y << " !l " << locv.z << "\n";
       //std::cerr << locv2.x << " !l " << locv2.y << " !l " << locv2.z << "\n";
+      GLfloat length = 50; //(locv2 - locv).magnitude_within_32_bits();
+      vertices_t* vertices = &verticeses[
+        tile_manhattan_distance_to_bounding_box_rounding_down(
+          bounding_box(p.first, p.first + (p.second / length)),
+          view_loc)
+      ];
       //begin quad
-      push_vertex(vertices.laserbeam, locv.x, locv.y, locv.z+0.5);
-      push_vertex(vertices.laserbeam, locv.x, locv.y, locv.z-0.5);
+      push_vertex(vertices->laserbeam, locv.x, locv.y, locv.z+0.5);
+      push_vertex(vertices->laserbeam, locv.x, locv.y, locv.z-0.5);
       //push_vertex(laserbeam_vertices, locv.x, locv.y, locv.z);
       //push_vertex(laserbeam_vertices, locv.x, locv.y, locv.z + 0.1);
-      GLfloat length = 50; //(locv2 - locv).magnitude_within_32_bits();
       for (int i = 0; i < length; ++i) {
         vector3<GLfloat> locv3 = (locv + (((locv2 - locv) * i) / length));
-        push_vertex(vertices.laserbeam, locv3.x, locv3.y, locv3.z);
-        push_vertex(vertices.laserbeam, locv3.x, locv3.y, locv3.z + 0.1);
+        push_vertex(vertices->laserbeam, locv3.x, locv3.y, locv3.z);
+        push_vertex(vertices->laserbeam, locv3.x, locv3.y, locv3.z + 0.1);
         //end quad
+        vertices = &verticeses[
+          tile_manhattan_distance_to_bounding_box_rounding_down(
+            bounding_box(p.first + (p.second * i / length), p.first + (p.second * (i+1) / length)),
+            view_loc)
+        ];
         //begin quad
-        push_vertex(vertices.laserbeam, locv3.x, locv3.y, locv3.z + 0.1);
-        push_vertex(vertices.laserbeam, locv3.x, locv3.y, locv3.z);
+        push_vertex(vertices->laserbeam, locv3.x, locv3.y, locv3.z + 0.1);
+        push_vertex(vertices->laserbeam, locv3.x, locv3.y, locv3.z);
       }
-      push_vertex(vertices.laserbeam, locv2.x, locv2.y, locv2.z);
-      push_vertex(vertices.laserbeam, locv2.x, locv2.y, locv2.z+0.1);
+      push_vertex(vertices->laserbeam, locv2.x, locv2.y, locv2.z);
+      push_vertex(vertices->laserbeam, locv2.x, locv2.y, locv2.z+0.1);
       //end quad
     }
     
