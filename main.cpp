a6c26590b2a622b97921d020033738d51bf2f6c1
Isaac Dupree <antispam@idupree.com>
2012-07-21 21:00:16 -0400

    Remove no-longer-needed include
    We recently changed the std::function arguments in a way that
    happened to make them complete here without the include.
    
    (Also, libc++ since decided to allow incomplete argument types
    in that place; the standard doesn't mandate this behavior
    but it is common and sensible for implementations to allow it.)

--- main.cpp
+++ main.cpp
@@ -69,8 +69,6 @@
 #include "world.hpp"
 #include "specific_worlds.hpp"
 #include "specific_object_types.hpp"
-#include "worldgen.hpp" //only so that world_building_gun is a complete type for
-  // http://stackoverflow.com/questions/10730682/does-stdfunctions-copy-constructor-require-the-template-types-argument-types
 
 #include "tests/test_main.hpp"
 
