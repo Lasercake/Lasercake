5302333484b0035ed1f258529fdb35dd9ef94042
Isaac Dupree <antispam@idupree.com>
2013-02-26 02:45:30 -0500

    Add trickily-missing Boost header files.

---
+++ generate-boostbcp-aux.hpp
@@ -0,0 +1,6 @@
+// This file is necessary because bcp gets confused by some of Boost's
+// preprocessor magic (#include SOME_MACRO_NAME()) and fails to copy
+// certain necessary files that we list here.
+
+#include <boost/typeof/incr_registration_group.hpp>
+
